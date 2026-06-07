2c4aec0be17f3af40a93b1a7298329c9fed74d11
Dipen Boro <borod9200@gmail.com>
2026-06-07 21:14:11 +0530

    added an ApiError

---
+++ src/utils/ApiError.js
@@ -0,0 +1,18 @@
+// structure api error class
+class ApiError extends Error {
+    constructor(statusCode, message, errors = [], stack = "") {
+        super(message)
+        this.statusCode = statusCode
+        this.errors = errors
+        this.sucess = false
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError }
+
