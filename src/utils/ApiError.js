24f94b0437bbece6091cfdd800319e2ab4a0ea18
Dipen Boro <borod9200@gmail.com>
2026-06-08 21:26:04 +0530

    fix ApiError & errors

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,10 +1,12 @@
 // structure api error class
-class ApiError extends Error {
-    constructor(statusCode, message, errors = [], stack = "") {
+
+class AppError extends Error {
+    constructor({ statusCode, message, code = "INTERNAL_ERROR", errors = [], stack = "") {
         super(message)
         this.statusCode = statusCode
         this.errors = errors
-        this.sucess = false
+        this.success = false
+        this.isOperational = true;
 
         if (stack) {
             this.stack = stack
@@ -14,5 +16,42 @@ class ApiError extends Error {
     }
 }
 
-export { ApiError }
+class BadRequestError extends AppError {
+    constructor(message = "Bad Request", code="BAD_REQEST", errors = []){
+        super({statusCode: 400, message, code, errors});
+    }
+}
+
+class UnauthorizedError extends AppError{
+    constructor(message = "Unauthorized", code="UNAUTHORIZED") {
+        super({ statusCode = 401, message, code });
+    }
+}
+
+class ForbiddenError extends AppError {
+    constructor(message="Forbidden", code = "FORBIDDEN" ) {
+        super({ statusCode: 403, message, code });
+    }
+}
+
+class NotFoundError extends AppError{
+    constructor({ message = "Resource not found", code = "NOT_FOUND" }) {
+        super({ statusCode: 404, message, code })
+    }
+}
+
+class ConflictError extends AppError {
+    constructor(message="Conflict occured", code = "CONFLICT_ERROR") {
+        super({ statusCode: 409, message, code });
+    }
+}
+
+class TooManyRequests extends AppError {
+    constructor(message = "Too many requests, pleae try again later", code = "TOO_MANY_REQEST" ) {
+        super({ statusCode: 429, message, code });
+    }
+}
+
+
+export { AppError, TooManyRequests, BadRequestError, ConflictError, UnauthorizedError, ForbiddenError, NotFoundError }
 
