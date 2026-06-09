ac0d9dcd8efd51b1e86adee02a0e41635d14a433
Dipen Boro <borod9200@gmail.com>
2026-06-09 07:40:54 +0530

    adding ApiResponse

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+class ApiResponse {
+    constructor(statusCode, message, data = null, meta = []) {
+        this.success = true
+        this.statusCode = statusCode
+        this.message = message
+        this.data = data
+        this.meta = {
+            timestamps: new Date().toISOString(),
+            unix: Date.now(),
+            meta,
+        }
+    }
+}
+
+export { ApiResponse };
